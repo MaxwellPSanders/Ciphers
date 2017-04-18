ce54b57fa649d623ac93fba7935407e1ed3f2e03
Maxwell Sanders <sanderspmaxwell@gmail.com>
2017-04-18 18:37:42 -0500

    Finished this and updated the readme

--- caesar/readme.txt
+++ caesar/readme.txt
@@ -4,3 +4,22 @@ One example being a caesar shift of 5 up would change a to f.
 This also means that there is wrapping that occurs.
 
 This will be compiled using gcc in a unix environment.
+
+Program can be run in two ways:
+
+print to screen:
+-------------------
+program # inputfile
+-------------------
+program - the name you compiled your program to
+# - the amount you want to shift up or down  
+inputfile - the name of the input file you want to shift
+
+print to file:
+------------------------------
+program # inputfile outputfile
+------------------------------
+program - the name you compiled your program to
+# - the amount you want to shift up or down  
+inputfile - the name of the input file you want to shift
+outputfile - the name of the file you want to output to
--- caesar/shift.c
+++ caesar/shift.c
@@ -14,9 +14,9 @@
  */
 int main(int argv, char** args){
     //check to see if there are 3 extra arguments
-    if(argv != 4){
+    if(argv != 4 && argv != 3){
         printf("Improper syntax\n");
-        printf("Program shift inputfile outputfile/\"print\"\n"); 
+        printf("Program shift inputfile outputfile/nothing\n"); 
         return 0;
     }
 
@@ -37,7 +37,7 @@ int main(int argv, char** args){
     }
 
     //check if the input file is the same as the output file
-    if(!strcmp(args[2],args[3])){
+    if(argv == 4 && !strcmp(args[2],args[3])){
         printf("Input file cannot match output file!\n");
         return 0;
     }
@@ -45,7 +45,7 @@ int main(int argv, char** args){
     char text[100]; // string to hold and encode
     //check to see if the user wants it printed to screen
     //otherwise try to open file and print to that
-    if(!strcmp(args[3], "print")){
+    if(argv == 3){
         while(fgets(text, sizeof(text) - 1, ifp) != NULL){
             //check to see if char is alphabetic, if so, shift it
             for(counter = 0; counter < 100; counter ++){
@@ -77,12 +77,42 @@ int main(int argv, char** args){
             }
             printf("%s", text);
         }
-        printf("\n");
         return 0;
     }
     else{
-  
-
+        // open output file
+        FILE *ofp = fopen(args[3], "w");
+        while(fgets(text, sizeof(text) - 1, ifp) != NULL){
+            //check to see if char is alphabetic, if so, shift it
+            for(counter = 0; counter < 100; counter ++){
+                //check if it is lowercase
+                if(islower(text[counter])){
+                    text[counter] += shift;
+                    //if it is now not lower, reshift depending on the operation
+                    if(!islower(text[counter])){
+                       if(shift >= 0){ 
+                           text[counter] -= 26;
+                       } 
+                       else{
+                           text[counter] += 26;
+                       }
+                    }
+                }
+                else if(isupper(text[counter])){
+                    text[counter] += shift;
+                    //if it is now not upper, reshift depending on the operation
+                    if(!isupper(text[counter])){
+                       if(shift >= 0){ 
+                           text[counter] -= 26;
+                       } 
+                       else{
+                           text[counter] += 26;
+                       }
+                    }
+                }
+            }
+            fprintf(ofp, "%s", text);
+        }
+        return 0;
     }
-    //place into output file
 }
